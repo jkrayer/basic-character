bcef11b2f1e93b4859c2a6e8219027f12a3e0e3a
jkrayer <jameskrayer@yahoo.com>
2019-01-07 22:30:27 -0500

    Add button to go to class List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,25 +8,34 @@
         @scores="handleScores"
       />
     </div>
-    <table v-if="scores.length > 0">
-      <thead>
-        <tr>
-          <td></td>
-          <th>Score</th>
-          <th>Modifier</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="score in scores"
-          :key="score[0]"
-        >
-          <th class="txt-left weight-400">{{ score[0] }}</th>
-          <td class="txt-center">{{ score[1] }}</td>
-          <td>{{ (score[2] > 0 ? '+' : '') + score[2] }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="scores.length > 0">
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <th>Score</th>
+            <th>Modifier</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="score in scores"
+            :key="score[0]"
+          >
+            <th class="txt-left weight-400">{{ score[0] }}</th>
+            <td class="txt-center">{{ score[1] }}</td>
+            <td>{{ (score[2] > 0 ? '+' : '') + score[2] }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button
+        type="button"
+        class="bg-blue txt-white weight-700 caps pad1 br"
+        @click="listClasses"
+      >
+        List Classes
+      </button>
+    </div>
     <p class="txt-smaller txt-right mt1">Version 1.1.0 (B<span class="txt-gray">ECMI</span>)</p>
   </div>
 </template>
@@ -51,6 +60,12 @@
       },
       handleScores(response) {
         this.scores = response.scores;
+      },
+      listClasses() {
+        this.$router.push({
+          path: '/basic/characters',
+          query: this.scores.reduce((acc, score) => (acc[score[0]] = score[1], acc), {})
+        });
       }
     }
   }
