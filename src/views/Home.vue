52df6177540cb89c05806c05de4ff919ece11f47
jkrayer <jameskrayer@yahoo.com>
2019-01-10 09:41:19 -0500

    Delete notes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,7 @@
   <div>
     <h1>B<span class="txt-gray">ECMI</span> Character Creator</h1>
     <router-link :to="{ name: 'create-character' }">New Character</router-link>
-    <pre>
-      Class
-        Cleric
-        Dwarf
-        Elf
-        Fighter
-        Halfling
-        Magic-User
-        Thief
-    </pre>
     <router-link :to="{ name: 'equipment' }">Equipment</router-link>
-    <pre>
-      Spells
-    </pre>
     <p class="txt-smaller txt-right mt1">Version 1.1.0</p>
   </div>
 </template>
