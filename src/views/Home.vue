d1406abdda9594b4c3b203238986e094834a449b
jkrayer <jameskrayer@yahoo.com>
2019-01-11 17:53:03 -0500

    Style home page buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>B<span class="txt-gray">ECMI</span> Character Creator</h1>
-    <router-link :to="{ name: 'create-character' }">New Character</router-link>
-    <router-link :to="{ name: 'equipment' }">Equipment</router-link>
+    <router-link :to="{ name: 'create-character' }" class="caps txt-center no-underline block txt-white pad1 br bg-blue mh1">New Character</router-link>
+    <router-link :to="{ name: 'equipment' }" class="caps txt-center no-underline block txt-white pad1 br bg-blue mh1">Equipment</router-link>
     <p class="txt-smaller txt-right mt1">Version 1.1.0</p>
   </div>
 </template>
