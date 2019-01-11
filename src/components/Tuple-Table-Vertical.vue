33a5824fc2d42c23760462ab3edef2a5542fe5aa
jkrayer <jameskrayer@yahoo.com>
2019-01-11 00:25:53 -0500

    Refactor to make table of any amount of rows

--- src/components/Tuple-Table-Vertical.vue
+++ src/components/Tuple-Table-Vertical.vue
@@ -2,28 +2,33 @@
   <table>
     <thead>
       <tr>
-        <th
-          v-for="d in data"
-        >{{d[0]}}
-        </th>
+        <th v-for="d in data" :key="hash(0, d)">{{ d[0] }}</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td
-          v-for="d in data"
-        >{{d[1]}}
-        </td>
+      <tr v-for="n in size()" :key="n">
+        <td v-for="d in data" :key="d[n]">{{ d[n] }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-export default {
-  name: 'TupleTableVerticle',
-  props: {
-    data: Array
-  }
-}
+  export default {
+    name: 'TupleTableVerticle',
+    props: {
+      data: {
+        type: Array,
+        required: true
+      }
+    },
+    methods: {
+      size() {
+        return this.data[0].length - 1;
+      },
+      hash(v, arr) {
+        return [v].concat(arr).join('');
+      }
+    }
+  };
 </script>
