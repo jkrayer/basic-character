05487f7954417a525536be2a3d6ec0cee5c7fb24
jkrayer <jameskrayer@yahoo.com>
2019-01-11 11:26:08 -0500

    Clean up

--- src/components/StripedTable.vue
+++ src/components/StripedTable.vue
@@ -2,49 +2,32 @@
   <table class="w100 mh1">
     <thead>
       <tr>
-        <th
-          v-for="(head, index) in header"
-          class="underline cell"
-          :class="index === 0 ? 'txt-left' : ''"
-          :key="head"
-        >
-          {{head}}
-        </th>
+        <th v-for="(head, index) in header" :key="head" class="underline cell" :class="index === 0 ? 'txt-left' : ''">{{head}}</th>
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="bod in body"
-        :key="bod.id"
-      >
+      <tr v-for="bod in body" :key="bod.id">
         <th class="txt-left weight-400 cell">{{bod.name}}</th>
-        <td
-          v-if="bod.ac || bod.damage"
-          class="txt-center cell"
-        >
-          {{bod.ac || bod.damage}}
-        </td>
+        <td v-if="bod.ac || bod.damage" class="txt-center cell">{{bod.ac || bod.damage}}</td>
         <td class="txt-center cell">{{bod.cost}} gp.</td>
-        <td
-          v-if="header.indexOf('Notes') > -1"
-          class="txt-center cell"
-        >
-          {{bod.notes}}
-        </td>
+        <td v-if="header.indexOf('Notes') > -1" class="txt-center cell">{{bod.notes}}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-export default {
-  name: 'StripedTable',
-  props: {
-    header: Array,
-    body: Array
-  }
-}
+  export default {
+    name: 'EquipmentTable',
+    props: {
+      header: {
+        type: Array,
+        required: true
+      },
+      body: {
+        type: Array,
+        required: true
+      }
+    },
+  };
 </script>
-
-<style>
-</style>
