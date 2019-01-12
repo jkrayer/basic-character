ac90467f9227e3002fb8f700c72cd1b771984a5b
jkrayer <jameskrayer@yahoo.com>
2019-01-12 13:57:38 -0500

    Add progress bar and start using list classes

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb1">
+    <span
+      v-for="(step, index) in steps"
+      :key="step"
+      :class="index === activeKey ? 'active' : index < activeKey ? 'strike' : 'txt-gray'"
+    >{{ step }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    steps: {
+      type: Array,
+      required: true
+    },
+    activeKey: Number
+  }
+};
+</script>
--- src/views/CreateCharacter.vue
+++ src/views/CreateCharacter.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>Basic D&amp;D Character Creator</h1>
+    <ProgressBar
+      :steps="['1. Enter Scores', '2. Pick a Class', '3. Buy Equipment']"
+      :activeKey="activeKey"
+    />
     <div v-if="scores.length === 0">
-      <p class="mb1">{{ message }}</p>
-      <ScoreForm
-        @submit="handleSubmit"
-      />
+      <p class="mb1">{{ message }}</p> <!-- TODO: move message into form -->
+      <ScoreForm @submit="handleSubmit" />
     </div>
-    <div v-if="scores.length > 0">
+
+    <div v-if="scores.length > 0 && !showCharacters">
+      <!-- TODO: Abstract this table into a component -->
       <table>
         <thead>
           <tr>
@@ -17,26 +21,17 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="score in scores"
-            :key="score[0]"
-          >
+          <tr v-for="score in scores" :key="score[0]">
             <th class="txt-left weight-400">{{ score[0] }}</th>
             <td class="txt-center">{{ score[1] }}</td>
             <td>{{ (score[2] > 0 ? '+' : '') + score[2] }}</td>
           </tr>
         </tbody>
       </table>
-      <button
-        type="button"
-        class="bg-blue txt-white weight-700 caps pad1 br"
-        @click="listClasses"
-      >
-        List Classes
-      </button>
+      <button type="button" class="bg-blue txt-white weight-700 caps pad1 br" @click="listClasses">List Classes</button>
     </div>
-    <div v-if="characters.length > 0">
-      <h1>Available Characters</h1>
+
+    <div v-if="showCharacters">
       <CharacterCard
         v-for="character in characters"
         :key="character.name"
@@ -49,6 +44,7 @@
 <script>
   import ScoreForm from '@/components/ScoreForm';
   import CharacterCard from '@/components/CharacterCard';
+  import ProgressBar from '@/components/ProgressBar';
   import server from '@/helpers/server';
   import valid from '@/helpers/validators';
 
@@ -67,13 +63,16 @@
     name: 'home',
     components: {
       CharacterCard,
-      ScoreForm
+      ScoreForm,
+      ProgressBar
     },
     data() {
       return {
         message: 'Enter scores to begin.',
         scores: [],
-        characters: []
+        characters: [],
+        activeKey: 0,
+        showCharacters: false
       }
     },
     methods: {
@@ -91,6 +90,8 @@
         }
       },
       listClasses() {
+        this.showCharacters = true;
+        this.activeKey = 1;
       }
     },
     created() {
